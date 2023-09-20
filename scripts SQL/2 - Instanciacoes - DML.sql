0eea8c84782e8e7cb029511e23378ca7f09fbbf6
pablo ramalho <pabloamaral512@gmail.com>
2023-09-20 18:16:51 -0300

    Definitivo?

--- scripts SQL/2 - Instanciacoes - DML.sql
+++ scripts SQL/2 - Instanciacoes - DML.sql
@@ -1,7 +1,6 @@
 -- operações de inserção de dados nas tabelas (instanciação de dados no banco)
 
 INSERT INTO AEROPORTO VALUES('Aeroporto Internacional de Brasilia', 1);
-                            
 
 
 INSERT INTO COMPANHIA_AEREA VALUES('Avianca', 1, 1),
@@ -10,11 +9,10 @@ INSERT INTO COMPANHIA_AEREA VALUES('Avianca', 1, 1),
                                   ('GOL', 4, 1);
                                   
 ALTER TABLE COMPANHIA_AEREA ADD FOREIGN KEY (ID_AEROPORTO) REFERENCES AEROPORTO(ID_AEROPORTO) ON DELETE SET NULL ON UPDATE CASCADE;
-                                  
 
 
 INSERT INTO FUNCIONARIO VALUES('Bruno Motta', 'brunom@gmail.com', '991813701', 1, 1),
-							  ('Marta Lopes', 'martalps@outlook.com', '984510293', 2, 1),
+                              ('Marta Lopes', 'martalps@outlook.com', '984510293', 2, 1),
                               ('Pedro Magalhaes', 'pmag@hotmail.com', '991403912', 3, 1),
                               ('Fernando Silva', 'fernandosilva@hotmail.com', '999801427', 4, 1),
                               ('Leonardo Freitas', 'leofreitas@gmail.com', '999718240', 5, 1),
@@ -27,9 +25,8 @@ INSERT INTO FUNCIONARIO VALUES('Bruno Motta', 'brunom@gmail.com', '991813701', 1
 ALTER TABLE FUNCIONARIO ADD FOREIGN KEY (ID_AEROPORTO) REFERENCES AEROPORTO(ID_AEROPORTO) ON DELETE CASCADE ON UPDATE CASCADE;
 
 
-
 INSERT INTO PASSAGEIRO VALUES(1, 'Kevin Silva', '1980-03-19', 'M', 'Brasileiro', 1),
-							 (2, 'Joana Santos', '1997-12-01', 'F', 'Portugues', 2),
+                             (2, 'Joana Santos', '1997-12-01', 'F', 'Portugues', 2),
                              (3, 'Carlos Gomes', '1968-01-15', 'M', 'Angolano', 3),
                              (4, 'Felipe Couto', '1998-03-09', 'M', 'Brasileiro', 4),
                              (5, 'Rafaela Morais', '1989-07-12', 'F', 'Brasileiro', 5),
@@ -48,20 +45,18 @@ INSERT INTO PASSAGEIRO VALUES(1, 'Kevin Silva', '1980-03-19', 'M', 'Brasileiro',
                              (18, 'Britney Santos', '1995-01-12', 'F', 'Brasileiro', 18),
                              (19, 'Julio Mendes', '1999-08-21', 'M', 'Brasileiro', 19),
                              (20, 'Fernanda Jobim', '1981-08-01', 'F', 'Angolano', 20);
-							
 
 
-INSERT INTO OPERADOR_DE_CARGA VALUES('Azul Cargo Express', 1),	-- 4 FUNCIONARIOS (alguns funcionários são operadores de carga)
-									('Avianca Cargo', 2),
+INSERT INTO OPERADOR_DE_CARGA VALUES('Azul Cargo Express', 1), -- 4 FUNCIONARIOS (alguns funcionários são operadores de carga)
+                                    ('Avianca Cargo', 2),
                                     ('LATAM Cargo', 3),
                                     ('Gollog', 4);
 
-ALTER TABLE OPERADOR_DE_CARGA ADD FOREIGN KEY (ID_FUNCIONARIO) REFERENCES FUNCIONARIO(ID_FUNCIONARIO) ON DELETE CASCADE ON UPDATE CASCADE;         
-
+ALTER TABLE OPERADOR_DE_CARGA ADD FOREIGN KEY (ID_FUNCIONARIO) REFERENCES FUNCIONARIO(ID_FUNCIONARIO) ON DELETE CASCADE ON UPDATE CASCADE;        
 
 
 INSERT INTO AVIAO VALUES('corredor unico', 1, 100, 1, 1),
-						('fuselagem larga', 2, 126, 2, 1),
+                        ('fuselagem larga', 2, 126, 2, 1),
                         ('fuselagem larga', 3, 132, 3, 1),
                         ('regional', 4, 80, 4, 1),
                         ('corredor duplo', 5, 180, 5, 2),
@@ -72,9 +67,8 @@ INSERT INTO AVIAO VALUES('corredor unico', 1, 100, 1, 1),
 ALTER TABLE AVIAO ADD FOREIGN KEY (ID_COMPANHIA) REFERENCES COMPANHIA_AEREA(ID_COMPANHIA) ON DELETE SET NULL ON UPDATE CASCADE;  
 
 
-
-INSERT INTO VOO VALUES('Nova Iorque', '2016-05-19', '17:35:01', 1, 5, 1, 1),    -- 8 AVIOES, 1 AEROPORTO, 4 OPERADORES DE CARGA (FUNCIONARIOS)
-					  ('Miami', '2021-04-07', '21:04:09', 2, 4, 1, 1),
+INSERT INTO VOO VALUES('Nova Iorque', '2016-05-19', '17:35:01', 1, 5, 1, 1), -- 8 AVIOES, 1 AEROPORTO, 4 OPERADORES DE CARGA (FUNCIONARIOS)
+                      ('Miami', '2021-04-07', '21:04:09', 2, 4, 1, 1),
                       ('Casablanca', '2015-01-15', '09:08:54', 3, 3, 1, 1),
                       ('Toronto', '2022-08-09', '10:07:48', 4, 2, 1, 1),
                       ('Lisboa', '2017-01-02', '15:08:07', 5, 7, 1, 1),
@@ -92,16 +86,17 @@ INSERT INTO VOO VALUES('Nova Iorque', '2016-05-19', '17:35:01', 1, 5, 1, 1),    
                       ('Londres', '2023-09-15', '21:04:05', 17, 1, 1, 3),
                       ('Roma', '2017-07-14', '17:01:23', 18, 3, 1, 3),
                       ('Sydney', '2016-03-28', '18:08:27', 19, 7, 1, 3),
-                      ('Istambul', '2021-05-20', '15:01:20', 20, 8, 1, 3),;
+                      ('Istambul', '2021-05-20', '15:01:20', 20, 8, 1, 3),
+                      ('Toquio', '2003-01-06', '23:09:04', 21, 4, 1, 2),
+                      ('Doha', '2015-05-28', '15:03:01', 22, 5, 1, 4);
 
 ALTER TABLE VOO ADD FOREIGN KEY (ID_AVIAO) REFERENCES AVIAO(ID_AVIAO) ON DELETE CASCADE ON UPDATE CASCADE;
 ALTER TABLE VOO ADD FOREIGN KEY (ID_AEROPORTO) REFERENCES AEROPORTO(ID_AEROPORTO) ON DELETE SET NULL ON UPDATE CASCADE;
 ALTER TABLE VOO ADD FOREIGN KEY (ID_FUNCIONARIO) REFERENCES FUNCIONARIO(ID_FUNCIONARIO) ON DELETE SET NULL ON UPDATE CASCADE;
 
 
-
 INSERT INTO BAGAGEM VALUES(14, 1, 'aceito', 1, 1, 2),
-						  (16, 3, 'despachado', 2, 2, 4),
+                          (16, 3, 'despachado', 2, 2, 4),
                           (13, 2, 'despachado', 3, 3, 1),
                           (22, 2, 'entregue', 4, 4, 5),
                           (15, 3, 'danificado', 5, 5, 17),
@@ -125,9 +120,8 @@ ALTER TABLE BAGAGEM ADD FOREIGN KEY (ID_PASSAGEIRO) REFERENCES PASSAGEIRO(ID_PAS
 ALTER TABLE BAGAGEM ADD FOREIGN KEY (ID_VOO) REFERENCES VOO(ID_VOO) ON DELETE CASCADE ON UPDATE CASCADE;
 
 
-
 INSERT INTO RESERVA_EMBARQUE VALUES('15:09:20', '2016-05-19', 'concluido', '15:30:00', '2016-05-19', 'concluido', 45, 1, 1, 1),
-								   ('16:20:01', '2021-04-07', 'concluido', '16:25:23', '2021-04-07', 'concluido', 32, 2, 2, 2),
+                                   ('16:20:01', '2021-04-07', 'concluido', '16:25:23', '2021-04-07', 'concluido', 32, 2, 2, 2),
                                    ('14:32:59', '2015-01-15', 'concluido', '14:40:28', '2015-01-15', 'em andamento', 60, 3, 3, 3),
                                    ('10:01:29', '2022-08-09', 'concluido', '10:07:11', '2022-08-09', 'prioritario', 58, 4, 4, 4),
                                    ('18:05:30', '2017-01-02', 'pendente', '18:10:34', '2017-01-02', 'aberto', 60, 5, 5, 5),
@@ -146,6 +140,25 @@ INSERT INTO RESERVA_EMBARQUE VALUES('15:09:20', '2016-05-19', 'concluido', '15:3
                                    ('18:40:43', '2017-07-14', 'concluido', '18:42:01', '2017-07-14', 'em andamento', 10, 18, 18, 18),
                                    ('15:10:06', '2016-03-28', 'pendente', '15:11:56', '2016-03-28', 'aberto', 32, 19, 19, 19),
                                    ('20:09:45', '2021-05-20', 'concluido', '20:15:46', '2021-05-20', 'concluido', 20, 20, 20, 20);
+                                    '00:30:00', '2003-01-05', 'concluido', '00:38:00', '2003-01-05', 'concluido', 40, 21, 21, 1),
+                                    ('00:30:00', '2003-01-05', 'concluido', '00:42:00', '2003-01-05', 'concluido', 41, 21, 21, 2),
+                                    ('00:30:00', '2003-01-05', 'concluido', '00:41:00', '2003-01-05', 'concluido', 50, 21, 21, 3),
+                                    ('00:30:00', '2003-01-05', 'concluido', '00:40:00', '2003-01-05', 'concluido', 10, 21, 21, 4),
+                                    ('00:30:00', '2003-01-05', 'concluido', '00:49:00', '2003-01-05', 'concluido', 28, 21, 21, 5),
+                                    ('00:30:00', '2003-01-05', 'concluido', '00:57:00', '2003-01-05', 'concluido', 32, 21, 21, 6),
+                                    ('00:30:00', '2003-01-05', 'concluido', '00:58:00', '2003-01-05', 'concluido', 48, 21, 21, 7),
+                                    ('00:30:00', '2003-01-05', 'concluido', '00:54:00', '2003-01-05', 'concluido', 39, 21, 21, 8),
+                                    ('00:30:00', '2003-01-05', 'concluido', '00:37:00', '2003-01-05', 'concluido', 51, 21, 21, 9),
+                                    ('00:30:00', '2003-01-05', 'concluido', '00:39:00', '2003-01-05', 'concluido', 34, 21, 21, 10),
+                                    '14:50:00', '2016-06-23', 'concluido', '15:02:00', '2016-06-23', 'concluido', 57, 22, 22, 5),
+                                    ('14:54:00', '2016-06-23', 'concluido', '15:01:00', '2016-06-23', 'concluido', 58, 22, 22, 6),
+                                    ('14:52:00', '2016-06-23', 'concluido', '15:01:53', '2016-06-23', 'concluido', 45, 22, 22, 7),
+                                    ('14:57:00', '2016-06-23', 'concluido', '15:02:47', '2016-06-23', 'concluido', 56, 22, 22, 8),
+                                    ('14:51:00', '2016-06-23', 'concluido', '15:01:22', '2016-06-23', 'concluido', 60, 22, 22, 9),
+                                    ('14:52:00', '2016-06-23', 'concluido', '15:02:01', '2016-06-23', 'concluido', 41, 22, 22, 10),
+                                    ('14:51:00', '2016-06-23', 'concluido', '15:00:40', '2016-06-23', 'concluido', 48, 22, 22, 11),
+                                    ('14:53:00', '2016-06-23', 'concluido', '15:01:53', '2016-06-23', 'concluido', 32, 22, 22, 12);
+
 
 ALTER TABLE RESERVA_EMBARQUE ADD FOREIGN KEY (ID_VOO) REFERENCES VOO(ID_VOO) ON DELETE CASCADE ON UPDATE CASCADE;
 ALTER TABLE RESERVA_EMBARQUE ADD FOREIGN KEY (ID_PASSAGEIRO) REFERENCES PASSAGEIRO(ID_PASSAGEIRO) ON DELETE CASCADE ON UPDATE CASCADE;
--- scripts SQL/3 - Consultas - SELECT.sql
+++ scripts SQL/3 - Consultas - SELECT.sql
@@ -1,1 +1,56 @@
 -- consultas nas tabelas criadas e demais operações - -> mínimo de 6 consultas no banco, sendo no mínimo 4 delas contendo junções entre tabelas
+USE AEROPORTO;
+
+SELECT * FROM COMPANHIA_AEREA;
+SELECT * FROM RESERVA_EMBARQUE;
+SELECT * FROM VOO;
+
+/* Consulta 1 - Liste todos os passageiros que 
+têm nacionalidade portuguesa */
+SELECT NOME_PASSAGEIRO FROM PASSAGEIRO 
+WHERE NACIONALIDADE = 'Portugues';
+
+/* Consulta 2 - Liste o nome e o número de telefone 
+dos funcionários que usam o hotmail como email */
+SELECT NOME_FUNCIONARIO, TELEFONE FROM FUNCIONARIO
+WHERE EMAIL LIKE '%@hotmail.com';
+
+/* Consulta 3 - Liste o nome, a data de nascimento 
+e o gênero dos passageiros 
+que pegaram mais de um voo */
+SELECT P.NOME_PASSAGEIRO, P.DATA_NASCIMENTO, P.GENERO
+FROM PASSAGEIRO AS P
+JOIN RESERVA_EMBARQUE AS RE 
+ON P.ID_PASSAGEIRO = RE.ID_PASSAGEIRO
+GROUP BY P.ID_PASSAGEIRO
+HAVING COUNT(RE.ID_VOO) > 1;
+
+/* Consulta 4 - Liste o tipo de avião, a empresa de carga
+e o nome do funcionário responsável por todos
+os voos em aviões com capacidade de assentos 
+maior do que 100 */
+SELECT A.TIPO_AVIAO, O.EMPRESA_CARGA, F.NOME_FUNCIONARIO
+FROM OPERADOR_DE_CARGA AS O
+JOIN FUNCIONARIO AS F 
+ON O.ID_FUNCIONARIO = F.ID_FUNCIONARIO
+JOIN VOO AS V 
+ON F.ID_FUNCIONARIO = V.ID_FUNCIONARIO
+JOIN AVIAO AS A 
+ON V.ID_AVIAO = A.ID_AVIAO
+WHERE A.CAPACIDADE_ASSENTOS > 100;
+
+/* Consulta 5 - Liste o nome e a nacionalidade de todos os passageiros 
+que têm o voo encerrado */
+SELECT P.NOME_PASSAGEIRO, P.NACIONALIDADE
+FROM PASSAGEIRO AS P
+JOIN RESERVA_EMBARQUE AS RE ON P.ID_PASSAGEIRO = RE.ID_PASSAGEIRO
+JOIN VOO AS V ON RE.ID_VOO = V.ID_VOO
+WHERE RE.STATUS_EMBARQUE = 'encerrado'
+ORDER BY P.NOME_PASSAGEIRO;
+
+/* Consulta 6 - Liste o destino de todos os voos que tenham 
+bagagem de peso igual a 16 */
+SELECT V.DESTINO
+FROM VOO AS V
+JOIN BAGAGEM AS B ON V.ID_VOO = B.ID_VOO
+WHERE B.PESO = 16;
